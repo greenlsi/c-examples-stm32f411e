42bbd331a5bc1dabaf459e6e9a2ea28dc0b23d6c
Unai Garcia <unaigarciaruizd@gmail.com>
2022-08-31 18:37:38 +0200

    prueba con FreeRTOS

---
+++ FreeRTOS_prueba/Core/Src/main.c
@@ -0,0 +1,485 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ UART_HandleTypeDef huart2;
+
+/* Definitions for BlinkLED */
+osThreadId_t BlinkLEDHandle;
+const osThreadAttr_t BlinkLED_attributes = {
+  .name = "BlinkLED",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityAboveNormal,
+};
+/* Definitions for Write_UART */
+osThreadId_t Write_UARTHandle;
+const osThreadAttr_t Write_UART_attributes = {
+  .name = "Write_UART",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Wait_Button */
+osThreadId_t Wait_ButtonHandle;
+const osThreadAttr_t Wait_Button_attributes = {
+  .name = "Wait_Button",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityRealtime,
+};
+/* Definitions for UARTQueue */
+osMessageQueueId_t UARTQueueHandle;
+const osMessageQueueAttr_t UARTQueue_attributes = {
+  .name = "UARTQueue"
+};
+/* Definitions for Button_Interrupt_Event */
+osEventFlagsId_t Button_Interrupt_EventHandle;
+const osEventFlagsAttr_t Button_Interrupt_Event_attributes = {
+  .name = "Button_Interrupt_Event"
+};
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+void StartBlinkLed(void *argument);
+void StartWriteUart(void *argument);
+void StartWaitButton(void *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+typedef struct {
+	char data[32];
+}MensajeUART;
+
+atomic_bool boton;
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+atomic_init(&boton, 0);
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of UARTQueue */
+  UARTQueueHandle = osMessageQueueNew (8, sizeof(MensajeUART), &UARTQueue_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of BlinkLED */
+  BlinkLEDHandle = osThreadNew(StartBlinkLed, NULL, &BlinkLED_attributes);
+
+  /* creation of Write_UART */
+  Write_UARTHandle = osThreadNew(StartWriteUart, NULL, &Write_UART_attributes);
+
+  /* creation of Wait_Button */
+  Wait_ButtonHandle = osThreadNew(StartWaitButton, NULL, &Wait_Button_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* Create the event(s) */
+  /* creation of Button_Interrupt_Event */
+  Button_Interrupt_EventHandle = osEventFlagsNew(&Button_Interrupt_Event_attributes);
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, Button_interrupt_Pin|Button_processed_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : Button_interrupt_Pin Button_processed_Pin */
+  GPIO_InitStruct.Pin = Button_interrupt_Pin|Button_processed_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : User_botton_Pin */
+  GPIO_InitStruct.Pin = User_botton_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(User_botton_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD4_Pin LD3_Pin LD5_Pin LD6_Pin */
+  GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_Pin == User_botton_Pin) {
+		osThreadFlagsSet(Wait_ButtonHandle, 1);
+	}
+}
+
+void ComprobarEstado (int estado){
+	if (estado){
+		Error_Handler();
+	}
+}
+
+MensajeUART Formato_Mensaje(int trigger, bool boton_pulsado){
+	MensajeUART mensaje;
+	snprintf(mensaje.data, sizeof(mensaje.data),"El botón está a %i (%i)", boton_pulsado, trigger);
+	return mensaje;
+}
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartBlinkLed */
+/**
+  * @brief  Function implementing the BlinkLED thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartBlinkLed */
+void StartBlinkLed(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(100);
+    if (atomic_load(&boton) == GPIO_PIN_RESET) {
+    	HAL_GPIO_WritePin(GPIOD, LD4_Pin, GPIO_PIN_SET);
+    }
+    osDelay(100);
+    HAL_GPIO_WritePin(GPIOD, LD4_Pin, GPIO_PIN_RESET);
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartWriteUart */
+/**
+* @brief Function implementing the Write_UART thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartWriteUart */
+void StartWriteUart(void *argument)
+{
+  /* USER CODE BEGIN StartWriteUart */
+  /* Infinite loop */
+  for(;;)
+  {
+	MensajeUART mensaje;
+	ComprobarEstado(osMessageQueueGet(UARTQueueHandle, &mensaje, 0, osWaitForever));
+	size_t tamaño_mensaje = strnlen(mensaje.data, sizeof(mensaje.data));
+	ComprobarEstado(HAL_UART_Transmit(&huart2, (uint8_t*) &mensaje.data, (uint16_t) tamaño_mensaje, 1000));
+    //osDelay(1);
+  }
+  /* USER CODE END StartWriteUart */
+}
+
+/* USER CODE BEGIN Header_StartWaitButton */
+/**
+* @brief Function implementing the Wait_Button thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartWaitButton */
+void StartWaitButton(void *argument)
+{
+  /* USER CODE BEGIN StartWaitButton */
+	int trigger = 0;
+	MensajeUART mensaje;
+  /* Infinite loop */
+  for(;;)
+  {
+	//Reaccionar solo a rising edge
+	EXTI->RTSR |= User_botton_Pin;
+	EXTI->FTSR &= ~User_botton_Pin;
+
+	HAL_GPIO_WritePin(GPIOE, Button_processed_Pin, GPIO_PIN_RESET);
+	osThreadFlagsWait(1, osFlagsWaitAny, osWaitForever);
+	HAL_GPIO_WritePin(GPIOE, Button_processed_Pin, GPIO_PIN_SET);
+    trigger++;
+
+    atomic_store(&boton, 1);
+    mensaje = Formato_Mensaje(trigger, true);
+    ComprobarEstado(osMessageQueuePut(UARTQueueHandle, &mensaje, 0, osWaitForever));
+
+    //Reccionar solo a falling edge
+    EXTI->FTSR |= User_botton_Pin;
+    EXTI->RTSR &= ~User_botton_Pin;
+
+    HAL_GPIO_WritePin(GPIOE, Button_processed_Pin, GPIO_PIN_RESET);
+	osThreadFlagsWait(1, osFlagsWaitAny, osWaitForever);
+	HAL_GPIO_WritePin(GPIOE, Button_processed_Pin, GPIO_PIN_SET);
+	trigger++;
+
+	 atomic_store(&boton, 0);
+	 mensaje = Formato_Mensaje(trigger, false);
+	 ComprobarEstado(osMessageQueuePut(UARTQueueHandle, &mensaje, 0, osWaitForever));
+  }
+  /* USER CODE END StartWaitButton */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
